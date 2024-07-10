924af9f094400fe69f50bc581df2f9d862afdaaf
Jakkaphob <jkpbeam@gmail.com>
2024-07-10 14:40:28 +0700

    feat: add algo exercise 10 07 as feature/beam

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,20 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  let result = [];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] == target) {
+        result.push(i);
+        result.push(j);
+        console.log("result:", result);
+        return result;
+      }
+    }
+  }
+  console.log("result:", result);
+  return result;
+}
+
+numbers = [2, 7, 11, 15];
+
+twoSum(numbers, 236);
